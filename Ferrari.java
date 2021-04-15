c4f4aae7985454f90db0b1db9826b344d8f5044b
SotaNarita <sata.narita@tokyo.supersoftware>
2021-04-15 19:03:43 +0900

    add new file

--- Ferrari.java
+++ Ferrari.java
@@ -14,13 +14,15 @@ public class Ferrari extends RacingCar {
 			frontUp = true;
 			System.out.println("処理前の車高: " + vehecleHeight + "mm");
 			vehecleHeight += 40;
-			System.out.println("処理後の車高: " + vehecleHeight + "mm\n");
+			System.out.println("処理後の車高: " + vehecleHeight + "mm");
+			System.out.println("現在の加速度: " + acceleration + "m/s^2\n");
 		} else {
 			acceleration = 5;
 			frontUp = false;
 			System.out.println("処理前の車高: " + vehecleHeight + "mm");
 			vehecleHeight -= 40;
-			System.out.println("処理後の車高: " + vehecleHeight + "mm\n");
+			System.out.println("処理後の車高: " + vehecleHeight + "mm");
+			System.out.println("現在の加速度: " + acceleration + "m/s^2\n");
 		}
 	}
 }
